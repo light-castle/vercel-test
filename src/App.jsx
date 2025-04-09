bc427adb59da4263c0172c46f8d36d6aa6e03b72
light-castle <152480587+light-castle@users.noreply.github.com>
2025-04-10 01:47:21 +0900

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function Welcome() {
+  return <h2>Welcome to my app</h2>;
+}
+
+function App() {
+  return (
+    <div>
+      <Welcome />
+      <p>This is my first React component.</p>
+    </div>
+  );
+}
+
+export default App;
